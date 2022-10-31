ab3e60ce7bb1d4af3d78b49a05439bffbe4e0789
jorditosa <jordisato88@gmail.com>
2022-10-31 18:59:08 +0100

    v9 afegint topbar

--- dist/articles/detall-1.de5c0784.js
+++
@@ -1,7 +0,0 @@
-// Menu hamburguesa
-const menuHamburguer = document.querySelector(".navbar");
-menuHamburguer.addEventListener("click", ()=>{
-    menuHamburguer.classList.toggle("clickHamburguer");
-});
-
-//# sourceMappingURL=detall-1.de5c0784.js.map
