72b026ae1bc366e700141e21a37e318e2d935767
jorditosa <jordisato88@gmail.com>
2022-11-08 17:16:51 +0100

    v20 revisando javascript y otros pequeños detalles del css

--- dist/detall-1.739bf03c.js
+++ dist/detall-1.739bf03c.js
@@ -580,7 +580,6 @@ exports.export = function(dest, destName, get) {
 // filter menu
 const navItem = document.querySelectorAll(".categories__nav--item");
 const galleryItem = document.querySelectorAll(".categories__item");
-for(let i = 0; i < navItem.length; i++)navItem[i].addEventListener("click", filterItem);
 // Seleccion del boton activo
 function setActiveItem(e) {
     // Quitar la clase active del elemento antiguo
@@ -590,31 +589,36 @@ function setActiveItem(e) {
         e.target.classList.add("active-link");
     });
 }
+function reseting() {
+    // Resetando el filtro
+    galleryItem.forEach((item)=>{
+        item.classList.add("item-visible");
+        item.classList.remove("item-hidden");
+    });
+}
 function filterItem(e) {
     // Activar elemento navegación seleccionado
     setActiveItem(e);
+    // Reseteando el filtro
+    reseting();
     // Iterar sobre los articulos
     galleryItem.forEach((item)=>{
-        item.classList.remove("item-hidden");
-        item.classList.add("item-visible");
         // Obtener valor atributo y clasificacion del articulo
         const itemType = item.dataset.img;
         // Obtener el valor del elemento del menu navegacion seleccionado
         const navType = e.target.dataset.btn;
         // Comenzar con el filtrado comparando valores
-        if (itemType !== navType) {
+        if (navType === "all") {
+            item.classList.add("item-visible");
+            item.classList.remove("item-hidden");
+        } else if (itemType !== navType) {
             item.classList.remove("item-visible");
             item.classList.add("item-hidden");
         }
     });
 }
-// configurando el evento click al elemento de nevagcion de Totes
-navItem[0].addEventListener("click", (e)=>{
-    setActiveItem(e);
-    galleryItem.forEach((item)=>{
-        item.classList.remove("item-hidden");
-        item.classList.add("item-visible");
-    });
+navItem.forEach((item)=>{
+    item.addEventListener("click", filterItem);
 });
 
 },{}],"3Agy5":[function(require,module,exports) {
--- src/js/assets/filtradoCat.js
+++ src/js/assets/filtradoCat.js
@@ -2,10 +2,6 @@
 const navItem = document.querySelectorAll('.categories__nav--item');
 const galleryItem = document.querySelectorAll('.categories__item');
 
-for (let i = 0; i < navItem.length; i++) {
-  navItem[i].addEventListener('click', filterItem);
-}
-
 // Seleccion del boton activo
 function setActiveItem(e) {
   // Quitar la clase active del elemento antiguo
@@ -17,34 +13,39 @@ function setActiveItem(e) {
   });
 }
 
+function reseting() {
+  // Resetando el filtro
+  galleryItem.forEach((item) => {
+    item.classList.add('item-visible');
+    item.classList.remove('item-hidden');
+  });
+}
+
 function filterItem(e) {
   // Activar elemento navegación seleccionado
   setActiveItem(e);
 
+  // Reseteando el filtro
+  reseting();
+
   // Iterar sobre los articulos
   galleryItem.forEach((item) => {
-    item.classList.remove('item-hidden');
-    item.classList.add('item-visible');
-
     // Obtener valor atributo y clasificacion del articulo
     const itemType = item.dataset.img;
     // Obtener el valor del elemento del menu navegacion seleccionado
     const navType = e.target.dataset.btn;
 
     // Comenzar con el filtrado comparando valores
-    if (itemType !== navType) {
+    if (navType === 'all') {
+      item.classList.add('item-visible');
+      item.classList.remove('item-hidden');
+    } else if (itemType !== navType) {
       item.classList.remove('item-visible');
       item.classList.add('item-hidden');
     }
   });
 }
 
-// configurando el evento click al elemento de nevagcion de Totes
-navItem[0].addEventListener('click', (e) => {
-  setActiveItem(e);
-
-  galleryItem.forEach((item) => {
-    item.classList.remove('item-hidden');
-    item.classList.add('item-visible');
-  });
+navItem.forEach((item) => {
+  item.addEventListener('click', filterItem);
 });
